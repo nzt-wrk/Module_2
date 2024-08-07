d31e8b395765538e6d49c875e31ca3421f0b9307
nzt-wrk <nzt.rus@gmail.com>
2024-08-07 14:44:15 +0300

    Add files via upload

--- module_2_2.py
+++ module_2_2.py
@@ -2,9 +2,18 @@ first = int(input('Введите первое число: '))
 second = int(input('Введите второе число: '))
 third = int(input('Введите третье число: '))
 
+# Вариант 1
 if first==second==third:
-    print('все числоа равны')
+    print('все числа равны')
 elif first==second or first==third  or second==third:
     print('два числа равны из трех')
 else:
     print('равных числе нет')
+
+# Вариант 2
+if first==second==third:
+    print('все числа равны')
+elif first!=second!=third:
+    print('равных числе нет')
+else:
+    print('два числа равны')
