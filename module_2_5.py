73f4a2e5b5b34c50625ed817a5a399d2d5a920f7
nzt-wrk <nzt.rus@gmail.com>
2024-08-10 00:51:21 +0300

    Add files via upload

--- module_2_5.py
+++ module_2_5.py
@@ -1,6 +1,5 @@
 def get_matrix(n, m, value):
     matrix = []
-    #set1 = []
     for i in range(n):
         matrix.append([])
         for j in range(m):
@@ -13,14 +12,3 @@ print('\n', get_matrix(4, 3, 11),
       '\n', get_matrix(3, 5, 3),
       '\n', get_matrix(1, 1, 5)
       )
-
-
-# print()
-
-# n = int(input("Кол-во строк: "))
-# m = int(input("Кол-во столбцов: "))
-# value = int(input("Значения матрицы: "))
-# result = get_matrix(n, m, value)
-#
-# print(result)
-
